5ca3091d58e619a5fb58ccfe59a9772fa6050026
ryan way <ryan.keith.way@gmail.com>
2020-04-03 23:06:06 -0700

    implement one and two bit chars tests

--- src/easy/oneAndTwoBitChars.cpp
+++ src/easy/oneAndTwoBitChars.cpp
@@ -2,8 +2,17 @@
  * Definition for a binary tree node.
  */
 #include "../../include/easy/oneAndTwoBitChars.hpp"
+#include <iostream>
 
 bool OneAndTwoBitChars::Solution::isOneBitCharacter(vector<int>& bits)
 {
-  return true;
+  for(int i = bits.size()-2; i > -1; i--)
+  {
+    if(bits[i] == 0)
+    {
+      int numOnes = bits.size() - i;
+      return numOnes % 2 == 0;
+    }
+  }
+  return (bits.size()-1) % 2 == 0;
 }
--- test/easy/oneAndTwoBitCharsTests.cpp
+++ test/easy/oneAndTwoBitCharsTests.cpp
@@ -1,7 +1,35 @@
 #include <gtest/gtest.h>
 #include "../../../include/easy/oneAndTwoBitChars.hpp"
 
-TEST(OneAndTwoBitCharsTests, Empty)
+TEST(OneAndTwoBitCharsTests, ListOfLengthOne)
 {
-  EXPECT_TRUE(true);
+  vector<int> in{ 0 };
+
+  bool exp = true;
+  OneAndTwoBitChars::Solution sol = OneAndTwoBitChars::Solution();
+
+  bool act = sol.isOneBitCharacter(in);
+  EXPECT_EQ(exp, act);
+}
+
+TEST(OneAndTwoBitCharsTests, Example1)
+{
+  vector<int> in{ 1, 0, 0 };
+
+  bool exp = true;
+  OneAndTwoBitChars::Solution sol = OneAndTwoBitChars::Solution();
+
+  bool act = sol.isOneBitCharacter(in);
+  EXPECT_EQ(exp, act);
+}
+
+TEST(OneAndTwoBitCharsTests, Example2)
+{
+  vector<int> in{ 1, 1, 1, 0 };
+  
+  bool exp = false;
+  OneAndTwoBitChars::Solution sol = OneAndTwoBitChars::Solution();
+
+  bool act = sol.isOneBitCharacter(in);
+  EXPECT_EQ(exp, act);
 }
