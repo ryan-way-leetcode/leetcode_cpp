d0f05dcee450c960a46217b0ab6dcf22c3b4fe1c
Ryan Way <ryan.keith.way@gmail.com>
2020-10-24 00:11:18 -0700

    1317: Convert Integer to the Sum of Two No-Zero Integers

--- src/easy/sumOfTwoNoZeroIntegers.cpp
+++ src/easy/sumOfTwoNoZeroIntegers.cpp
@@ -1,5 +1,34 @@
 #include "../../include/easy/sumOfTwoNoZeroIntegers.hpp"
 
-vector<int> SumOfTwoNoZeroIntegers::Solution::getNoZeroIntegers(int n) {
-  return vector<int>{ 2, 0 };
+bool SumOfTwoNoZeroIntegers::Solution::CheckIfNoZeroInteger(int value)
+{
+  int j;
+  for(j = 10; j <= value; j*=10);
+  for(; j > 1; j /= 10)
+  {
+    if((value/(j/10))%10 == 0)
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+vector<int> SumOfTwoNoZeroIntegers::Solution::getNoZeroIntegers(int n)
+{   
+  vector<int> ret = { 0, n };
+  while(ret[1] != 0)
+  {
+    if(this->CheckIfNoZeroInteger(ret[0]) && this->CheckIfNoZeroInteger(ret[1]))
+    {
+      return ret;
+    }
+    else
+    {
+      ret[0]++;
+      ret[1]--;
+    }
+  }
+  return ret;
 }
--- test/easy/sumOfTwoNoZeroIntegersTests.cpp
+++ test/easy/sumOfTwoNoZeroIntegersTests.cpp
@@ -1,37 +1,71 @@
 #include <gtest/gtest.h>
 #include "../../../include/easy/sumOfTwoNoZeroIntegers.hpp"
 
+void CheckIfNoZeroInteger(int value)
+{
+  int j;
+  for(j = 10; j <= value; j*=10);
+  for(; j > 1; j/=10)
+  {
+    EXPECT_FALSE((value/(j/10))%10 == 0) 
+      << "Expected No-Zero Integer. Got: " << value 
+      << ". Failed on: " << j;
+  }
+}
+
+void ValidateAnswer(vector<int> act, int n)
+{
+  ASSERT_TRUE(act.size() == 2) << "ExpectedSize: 2, Actual Size:" << act.size();
+  EXPECT_EQ(act[0] + act[1], n);
+  CheckIfNoZeroInteger(act[0]);
+  CheckIfNoZeroInteger(act[1]);
+}
+
 TEST(SumOfTwoNoZeroIntegers, Example1)
 {
   int n = 2;
   SumOfTwoNoZeroIntegers::Solution s = SumOfTwoNoZeroIntegers::Solution();
   
-  vector<int> act = s.getNoZeroIntegers(n);
-
-  ASSERT_TRUE(act.size() == 2);
-  EXPECT_EQ(act[0] + act[1], n);
-
-  for(int i = 0; i < act.size(); i++)
-  {
-    for(int j = 1; j <= act[i]*10; j*=10)
-    {
-      EXPECT_FALSE((act[i]/j)%10 != 0);
-    }
-  }
+  ValidateAnswer(s.getNoZeroIntegers(n), n);
 }
 
 TEST(SumOfTwoNoZeroIntegers, Example2)
 {
+  int n = 10000;
+  SumOfTwoNoZeroIntegers::Solution s = SumOfTwoNoZeroIntegers::Solution();
+  
+  ValidateAnswer(s.getNoZeroIntegers(n), n);
 }
 
 TEST(SumOfTwoNoZeroIntegers, Example3)
 {
+  int n = 69;
+  SumOfTwoNoZeroIntegers::Solution s = SumOfTwoNoZeroIntegers::Solution();
+  
+  ValidateAnswer(s.getNoZeroIntegers(n), n);
 }
 
 TEST(SumOfTwoNoZeroIntegers, Example4)
 {
+  int n = 1010;
+  SumOfTwoNoZeroIntegers::Solution s = SumOfTwoNoZeroIntegers::Solution();
+  
+  ValidateAnswer(s.getNoZeroIntegers(n), n);
 }
 
 TEST(SumOfTwoNoZeroIntegers, Example5)
 {
+  int n = 11;
+  SumOfTwoNoZeroIntegers::Solution s = SumOfTwoNoZeroIntegers::Solution();
+  
+  ValidateAnswer(s.getNoZeroIntegers(n), n);
+}
+
+TEST(SumofTwoNoZeroIntegers, ALL)
+{
+  SumOfTwoNoZeroIntegers::Solution s = SumOfTwoNoZeroIntegers::Solution();
+  for(int i = 2; i <= 10000; i++)
+  {
+    ValidateAnswer(s.getNoZeroIntegers(i), i);
+  }
 }
