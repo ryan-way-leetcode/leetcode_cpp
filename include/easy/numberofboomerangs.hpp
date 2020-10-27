77c185e6db36b07beb5c17df096ba5986d600c09
Ryan Way <ryan.keith.way@gmail.com>
2020-10-26 20:47:30 -0700

    447: beginning O2_Fast, all tests failing

--- include/easy/numberofboomerangs.hpp
+++ include/easy/numberofboomerangs.hpp
@@ -3,7 +3,10 @@
 
 #include <vector>
 #include <map>
-#include <math.h>
+#include <set>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,7 +15,9 @@ namespace NumberOfBoomerangs {
   class Solution {
     public:
       int numberOfBoomerangs(vector<vector<int>>& points);
-      int numberOfBoomerangs_Old(vector<vector<int>>& points);
+      int numberOfBoomerangs_O2_Fast(vector<vector<int>>& points);
+      int numberOfBoomerangs_O2(vector<vector<int>>& points);
+      int numberOfBoomerangs_O3(vector<vector<int>>& points);
     private:
       double distance(vector<int>, vector<int>);
   };
--- src/easy/numberofboomeranges.cpp
+++ src/easy/numberofboomeranges.cpp
@@ -7,6 +7,53 @@ double NumberOfBoomerangs::Solution::distance(
   return sqrt(pow(point1[0]-point2[0], 2) + pow(point1[1]-point2[1], 2));
 }
 
+/*
+ *
+ */
+int NumberOfBoomerangs::Solution::numberOfBoomerangs(
+    vector<vector<int>>& points)
+{
+  return this->numberOfBoomerangs_O2_Fast(points);
+}
+
+int NumberOfBoomerangs::Solution::numberOfBoomerangs_O2_Fast(
+    vector<vector<int>>& points)
+{
+  set<pair<int, int>> sPoints = set<pair<int, int>>();
+  for(vector<vector<int>>::iterator it = points.begin();
+      it != points.end(); it++)
+  {
+    sPoints.insert(pair<int, int>((*it)[0], (*it)[1]));
+  }
+
+  int count = 0;
+  for(set<pair<int, int>>::iterator i = sPoints.begin();
+      i != sPoints.end(); i++)
+  {
+    set<pair<int, int>> counted_distances = set<pair<int, int>>();
+    for(set<pair<int, int>>::iterator j = std::next(i);
+        j != sPoints.end(); j++)
+    {
+      pair<int, int> distance =
+        pair<int, int>(std::abs(i->first-j->first), std::abs(i->second-j->second));
+
+      if(counted_distances.count(distance) == 0)
+      {
+        counted_distances.insert(distance);
+        int num_this_distance = 
+         counted_distances.count(pair<int, int>(i->first+distance.first, i->second+distance.second)) +
+         counted_distances.count(pair<int, int>(i->first-distance.first, i->second+distance.second)) +
+         counted_distances.count(pair<int, int>(i->first+distance.first, i->second-distance.second)) +
+         counted_distances.count(pair<int, int>(i->first-distance.first, i->second-distance.second)); 
+
+        count += num_this_distance*(num_this_distance-1);
+      } 
+    }
+  }
+
+  return count;
+}
+
 /* for each point, calculate the number of points
  * at a given distance
  *
@@ -14,7 +61,7 @@ double NumberOfBoomerangs::Solution::distance(
  *  calculate the number of possible boomerangs
  *
  */
-int NumberOfBoomerangs::Solution::numberOfBoomerangs(
+int NumberOfBoomerangs::Solution::numberOfBoomerangs_O2(
     vector<vector<int>>& points)
 {
   //will contain for each point p, create a map of distance d to number of 
@@ -22,13 +69,15 @@ int NumberOfBoomerangs::Solution::numberOfBoomerangs(
   map<pair<int, int>, map<double, int>> distances = 
    map<pair<int, int>, map<double, int>>(); 
 
-  for(int i = 0; i < points.size(); i++)
+  for(vector<vector<int>>::iterator i = points.begin();
+      i != points.end(); i++)
   {
-    for(int j = i+1; j < points.size(); j++)
+    for(vector<vector<int>>::iterator j = i+1; 
+        j != points.end(); j++)
     {
-      pair<int, int> pair_i = pair<int, int>(points[i][0], points[i][1]);
-      pair<int, int> pair_j = pair<int, int>(points[j][0], points[j][1]);
-      double distance = this->distance(points[i], points[j]);
+      pair<int, int> pair_i = pair<int, int>((*i)[0], (*i)[1]);
+      pair<int, int> pair_j = pair<int, int>((*j)[0], (*j)[1]);
+      double distance = this->distance(*i, *j);
 
       if(distances.count(pair_j) == 0)
       {
@@ -52,7 +101,7 @@ int NumberOfBoomerangs::Solution::numberOfBoomerangs(
   }
 
   int count = 0;
-  
+  int max = 0; 
   for(map<pair<int, int>, map<double, int>>::iterator i = distances.begin();
       i != distances.end(); i++)
   {
@@ -60,14 +109,18 @@ int NumberOfBoomerangs::Solution::numberOfBoomerangs(
        j != i->second.end(); j++) 
     {
       count += j->second*(j->second-1);
+      if(j->first > max)
+      {
+        max = j->first;
+      }
     }
   }
-
+  cout << max << endl;
   return count;
 }
 
 //O(n^3)
-int NumberOfBoomerangs::Solution::numberOfBoomerangs_Old(
+int NumberOfBoomerangs::Solution::numberOfBoomerangs_O3(
     vector<vector<int>>& points)
 {
   int count = 0;
