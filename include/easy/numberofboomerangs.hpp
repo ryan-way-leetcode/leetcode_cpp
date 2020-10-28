554a16bd98b25e458a39fe88cc52e98fbac1d569
Ryan Way <ryan.keith.way@gmail.com>
2020-10-27 22:42:24 -0700

    447: clean up, passing tests. problem done

--- include/easy/numberofboomerangs.hpp
+++ include/easy/numberofboomerangs.hpp
@@ -15,11 +15,10 @@ namespace NumberOfBoomerangs {
   class Solution {
     public:
       int numberOfBoomerangs(vector<vector<int>>& points);
-      int numberOfBoomerangs_O2_Fast(vector<vector<int>>& points);
       int numberOfBoomerangs_O2(vector<vector<int>>& points);
       int numberOfBoomerangs_O3(vector<vector<int>>& points);
     private:
-      double distance(vector<int>, vector<int>);
+      int distance (vector<int>&, vector<int>&) const;
   };
 
 }
--- src/easy/numberofboomeranges.cpp
+++ src/easy/numberofboomeranges.cpp
@@ -1,10 +1,10 @@
 #include "../../include/easy/numberofboomerangs.hpp"
 
-double NumberOfBoomerangs::Solution::distance(
-    vector<int> point1,
-    vector<int> point2)
+int NumberOfBoomerangs::Solution::distance(
+    vector<int>& point1,
+    vector<int>& point2) const 
 {
-  return sqrt(pow(point1[0]-point2[0], 2) + pow(point1[1]-point2[1], 2));
+  return pow(point1[0]-point2[0], 2) + pow(point1[1]-point2[1], 2);
 }
 
 /*
@@ -14,57 +14,6 @@ int NumberOfBoomerangs::Solution::numberOfBoomerangs(
     vector<vector<int>>& points)
 {
   return this->numberOfBoomerangs_O2(points);
-}
-
-int NumberOfBoomerangs::Solution::numberOfBoomerangs_O2_Fast(
-    vector<vector<int>>& points)
-{
-  set<vector<int>> sPoints = set<vector<int>>();
-  for(vector<vector<int>>::iterator it = points.begin();
-      it != points.end(); it++)
-  {
-    sPoints.insert(*it);
-  }
-
-  int count = 0;
-  for(set<vector<int>>::iterator i = sPoints.begin();
-      i != sPoints.end(); i++)
-  {
-    set<vector<int>> counted_distances = set<vector<int>>();
-    for(set<vector<int>>::iterator j = sPoints.begin();
-        j != sPoints.end(); j++)
-    {
-      if (i == j) continue;
-      
-      vector<int> distance = { 
-        std::abs((*i)[0]-(*j)[0]), 
-        std::abs((*i)[1]-(*j)[1])
-      };
-
-      if(counted_distances.count(distance) == 0)
-      {
-        counted_distances.insert(distance);
-        counted_distances.insert({ distance[1], distance[0] });
-        int num_this_distance = 
-         sPoints.count({ (*i)[0]+distance[0], (*i)[1]+distance[1] }) +
-         (distance[0] ? sPoints.count({ (*i)[0]-distance[0], (*i)[1]+distance[1] }) : 0) +
-         (distance[1] ? sPoints.count({ (*i)[0]+distance[0], (*i)[1]-distance[1] }) : 0) +
-         (distance[0] && distance[1] ? sPoints.count({ (*i)[0]-distance[0], (*i)[1]-distance[1] }) : 0); 
-
-        if (distance[0] != distance[1])
-          num_this_distance += 
-            sPoints.count({ (*i)[0]+distance[1], (*i)[1]+distance[0] }) +
-            (distance[1] ? sPoints.count({ (*i)[0]-distance[1], (*i)[1]+distance[0] }) : 0) +
-            (distance[0] ? sPoints.count({ (*i)[0]+distance[1], (*i)[1]-distance[0] }) : 0) +
-            (distance[1] && distance[0] ? sPoints.count({ (*i)[0]-distance[1], (*i)[1]-distance[0] }) : 0); 
-
-
-        count += num_this_distance*(num_this_distance-1);
-      } 
-    }
-  }
-
-  return count;
 }
 
 /* for each point, calculate the number of points
@@ -81,30 +30,22 @@ int NumberOfBoomerangs::Solution::numberOfBoomerangs_O2(
   //points that are d distance from p
 
   int count = 0;
-  for(vector<vector<int>>::iterator i = points.begin();
-      i != points.end(); i++)
+  for(int i = 0; i != points.size(); i++)
   {
-    map<double, int> distances = map<double, int>();
-    for(vector<vector<int>>::iterator j = points.begin(); 
-        j != points.end(); j++)
+    map<int, int> distances = map<int, int>();
+    for(int j = 0; j != points.size(); j++)
     {
-      if(*j == *i) continue;
+      if(j == i) continue;
 
-      double distance = this->distance(*i, *j);
+      int distance = this->distance(points[i], points[j]);
 
-      if(distances.count(distance) == 0)
-      {
-        distances[distance] = 0;
-      }
-      
       distances[distance] += 1; 
 
     }
 
-    for(map<double, int>::iterator j = distances.begin();
-        j != distances.end(); j++)
+    for(auto &[key, value] : distances)
     {
-      count += j->second*(j->second-1);
+      count += value*(value-1);
     }
   }
 
