1ebf18d7a3c8d0f34d3dd2ebcc1eec82ec2bb492
Ryan Way <ryan.keith.way@gmail.com>
2020-10-31 11:13:02 -0700

    228: passing all tests, submission accepted

--- src/easy/228summaryranges.cpp
+++ src/easy/228summaryranges.cpp
@@ -1,7 +1,27 @@
 #include "../../include/easy/228summaryranges.hpp"
 
-vector<string> SummaryRanges::Solution::summaryRanges(
-  vector<int>& nums)
+vector<string> SummaryRanges::Solution::summaryRanges(vector<int>& nums)
 {
-  return vector<string>{ "Hello" };
+  if (nums.size() == 0) return {};
+  
+  vector<string> ret = {};
+  int range_start = 0;
+  
+  for(int i = 1; i < nums.size(); i++)
+  {
+    if (nums[i] != nums[i-1]+1)
+    {
+      ret.push_back(to_string(nums[range_start]) +
+          (range_start == i-1 ? ""
+          : "->" + to_string(nums[i-1])));
+
+      range_start = i;
+    } 
+  }
+
+  ret.push_back(to_string(nums[range_start]) + 
+      (range_start == nums.size()-1 ? ""
+      : "->" + to_string(nums[nums.size()-1])));
+  
+  return ret;
 }
