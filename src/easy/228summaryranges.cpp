9d3bbb99b95ded5a6be7de0fcb2c97497edc5846
Ryan Way <ryan.keith.way@gmail.com>
2020-11-02 05:41:50 -0800

    228: clean up

--- src/easy/228summaryranges.cpp
+++ src/easy/228summaryranges.cpp
@@ -2,9 +2,9 @@
 
 vector<string> SummaryRanges::Solution::summaryRanges(vector<int>& nums)
 {
-  if (nums.size() == 0) return {};
-  
   vector<string> ret = {};
+  
+  if (nums.size() == 0) return ret; 
   int range_start = 0;
   
   for(int i = 1; i < nums.size(); i++)
@@ -12,16 +12,14 @@ vector<string> SummaryRanges::Solution::summaryRanges(vector<int>& nums)
     if (nums[i] != nums[i-1]+1)
     {
       ret.push_back(to_string(nums[range_start]) +
-          (range_start == i-1 ? ""
-          : "->" + to_string(nums[i-1])));
+            (range_start == i-1 ? "" : "->" + to_string(nums[i-1])));
 
       range_start = i;
     } 
   }
 
   ret.push_back(to_string(nums[range_start]) + 
-      (range_start == nums.size()-1 ? ""
-      : "->" + to_string(nums[nums.size()-1])));
+      (range_start == nums.size()-1 ? "" : "->" + to_string(*(nums.end()-1))));
   
   return ret;
 }
