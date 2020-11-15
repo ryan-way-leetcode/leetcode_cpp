c13679c499ac120472a53dacf8bd1d451eb0f4bc
Ryan Way <ryan.keith.way@gmail.com>
2020-11-14 19:57:35 -0800

    345: submission accepted

--- src/easy/345ReverseVowels.cpp
+++ src/easy/345ReverseVowels.cpp
@@ -2,7 +2,24 @@
 
 string ReverseVowels::Solution::reverseVowels(string s)
 {
-  return s;
+  string ret(s);
+  string::reverse_iterator back = s.rbegin();
+  
+  for(string::iterator forward = ret.begin(); forward != ret.end(); forward++)
+  {
+    if(*forward == 'a' || *forward == 'e' || *forward == 'i' || *forward == 'o'
+        || *forward == 'u' || *forward == 'A' || *forward == 'E' 
+        || *forward == 'I' || *forward == 'O' || *forward == 'U' )
+    {
+      while(*back != 'a' && *back != 'e' && *back != 'i' && *back != 'o'
+        && *back != 'u' && *back != 'A' && *back != 'E' 
+        && *back != 'I' && *back != 'O' && *back != 'U' )
+        back++;
+      *forward = *back;
+      back++;
+    }
+  }
+  return ret;
 }
 
 
