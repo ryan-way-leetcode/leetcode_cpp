8f92c3182acebd604d9b11636d51f1091c9fea72
Ryan Way <ryan.keith.way@gmail.com>
2020-11-16 07:35:16 -0800

    349: problem setup

---
+++ include/easy/349IntersectionofTwoArrays.hpp
@@ -0,0 +1,16 @@
+#ifndef INTERSECTIONOFTWOARRAYS
+#define INTERSECTIONOFTWOARRAYS
+
+#include <vector>
+
+using namespace std;
+
+namespace IntersectionofTwoArrays {
+  
+  class Solution {
+    public:
+      vector<int> intersection(vector<int>& nums1, vector<int>& nums2);
+  };
+
+}
+#endif
---
+++ src/easy/349IntersectionofTwoArrays.cpp
@@ -0,0 +1,10 @@
+#include "../../include/easy/349IntersectionofTwoArrays.hpp"
+
+vector<int> IntersectionofTwoArrays::Solution::intersection(
+    vector<int>& nums1,
+    vector<int>& nums2)
+{
+  return {};
+}
+
+
