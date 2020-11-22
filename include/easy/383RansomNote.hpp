c9b5c17e5ae858b8888fc188f9228977e63fd6ec
Ryan Way <ryan.keith.way@gmail.com>
2020-11-22 05:39:45 -0800

    383: initial problem setup

---
+++ include/easy/383RansomNote.hpp
@@ -0,0 +1,16 @@
+#ifndef RANSOMNOTE
+#define RANSOMNOTE
+
+#include <string>
+
+using namespace std;
+
+namespace RansomNote {
+  
+  class Solution {
+    public:
+      bool canConstruct(string ransomNote, string magazine);
+  };
+
+}
+#endif
---
+++ src/easy/383RansomNote.cpp
@@ -0,0 +1,6 @@
+#include "../../include/easy/383RansomNote.hpp"
+
+bool RansomNote::Solution::canConstruct(string ransomNote, string magazine)
+{
+  return true;
+}
