5e2b596452752a6f831ed5b61957b05e15a2b9b4
Ryan Way <ryan.keith.way@gmail.com>
2020-11-29 22:20:43 -0800

    434: comments

--- src/easy/434NumberOfSegmentsInAString.cpp
+++ src/easy/434NumberOfSegmentsInAString.cpp
@@ -2,13 +2,18 @@
 
 int NumberOfSegmentsInAString::Solution::countSegments(string s)
 {
+  //set to true if current char is a word character
   bool character = false;
+  //count number of characters trailed by a space
   int count = 0;
   for(const char& c : s)
   {
+    //if current char is a space and previous character was not, increment
     count += c == ' ' && character;
+    //set flag if current character is not a space
     character = c != ' ';
   }
+  //covers edge case where the string ends in a word
   count += character;
 
   return count;
