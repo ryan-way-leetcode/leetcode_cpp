4509e8dc3334ccb663c915a64f9ca2a9ff67b397
Ryan Way <ryan.keith.way@gmail.com>
2020-11-29 22:08:42 -0800

    434: remove unneeded special case

--- src/easy/434NumberOfSegmentsInAString.cpp
+++ src/easy/434NumberOfSegmentsInAString.cpp
@@ -2,11 +2,6 @@
 
 int NumberOfSegmentsInAString::Solution::countSegments(string s)
 {
-  if (!any_of(s.begin(), s.end(), [](const char& c) { return c != ' '; }))
-  {
-    return 0;
-  }
-  
   bool character = false;
   int count = 0;
   for(const char& c : s)
